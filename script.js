6833c2d26b5c8335a4e9e9ffa8bec2f8c58ca4a7
Ostapchuk Daniil <ostapman228@gmail.com>
2025-02-07 19:47:43 +0400

    Лаба 4

--- script.js
+++ script.js
@@ -1,83 +1,42 @@
 // Задание №1
-const student = {
-    fname: "Иван",
-    sname: "Иванов",
-    marks:[
-        {
-            subject:"Физика",
-            mark: 5,
-        },
-        {
-            subject:"Информатика",
-            mark: 5,
-        },
-        {
-            subject:"Математика",
-            mark: 2,
-        },       
-    ]
-}
+class Figure{
+    #x;
+    #y;
+    constructor(x,y){
+        this.x=x;
+        this.y=y;
+    }
 
-function getInfo(student){
-    console.log('Имя: ' + student.fname)
-    console.log("Фамилия: " + student.sname)
-    student.marks.forEach(element => {
-        console.log(element.subject + " " + element.mark)
-    });
+    square(){
+        return undefined
+    }
 }
-
-getInfo(student)
 
 // Задание №2
-
-class Student {
-    constructor(fname,sname, marks){
-        this.fname = fname;
-        this.sname = sname;
-        this.marks = marks;
+class Circle extends Figure{
+    constructor(x,y,r){
+        super(x,y);
+        this.r=r;
     }
-
-    // Задание №3
-    average() {
-        let sum = 0;
-        let count = 0;
-        for (let element of this.marks){
-            sum+=element.mark
-            count+=1
-        }
-        return sum/count;
-    }
-
-    // Задание №4
-    getMarkForSubject(subject){
-        let a = []
-        this.marks.forEach(element => {if(element.subject === subject) {a.push(element.mark)}})
-        return a
-    }
-
-    // Задание №5
-    addMarkForSubject(subject,mark){
-        this.marks.push({subject,mark})
-    }
-
-    // Задание №6 (но говорят, что delete использовать - плохой тон, поэтому дальше другой способ)
-    // deleteMarksForSubjects(subject){
-    //     for (let i = 0; i<this.marks.length; i++){
-    //         if (this.marks[i].subject === subject){
-    //             delete this.marks[i]
-    //         }
-    //     }
-    // }
-
-    deleteMarksForSubject(subject){
-        let newArrayMarks = [];
-        this.marks.forEach(element => {if(element.subject !== subject){newArrayMarks.push(element)}});
-        this.marks = newArrayMarks
+    square(){
+        return Math.PI*this.r**2
     }
 }
-class Mark {
-    constructor(subject,mark){
-        this.subject = subject;
-        this.mark = mark;
+
+// Задание №3
+class Rectangle extends Figure{
+    constructor(x,y,h,w){
+        super(x,y);
+        this.h=h;
+        this.w=w;
+    }
+    square(){
+        return this.h*this.w
     }
 }
+
+// Задание №4
+const c = new Circle(1,1,2);
+console.log(c.square())
+const r = new Rectangle(1,1,2,3)
+console.log(r.square())
