6e8299d9789b357e0e1c836ec9ce1248ffef8fe6
Ostapchuk Daniil <ostapman228@gmail.com>
2025-02-09 00:38:43 +0400

    Лаба №7

--- script.js
+++ script.js
@@ -1,60 +1,47 @@
 // Задание №1
-
-function findSame(firstAr, secondAr){
-    let sameValue = new Set();
-    let difValue = new Set();
-
-    for (let element of firstAr){
-        difValue.add(element)
+class Student {
+    constructor(fname,sname, marks){
+        this.fname = fname;
+        this.sname = sname;
+        this.marks = marks;
     }
 
-    for (let element of secondAr){
-        if (difValue.has(element)){
-            sameValue.add(element)
+    average() {
+        let sum = 0;
+        let count = 0;
+        for (let element of this.marks){
+            sum+=element.mark
+            count+=1
         }
-    }
-
-    return sameValue;
-}
-
-const ar1 = [4,6,9,4436,74,2,4]
-const ar2 = [4,6,7,11,4,23,435,4]
-console.log(findSame(ar1,ar2))
-
-// Задание №2
-function countValue(array){
-    let dictionary = new Map();
-    array.forEach(element => dictionary.get(element)!==undefined ? dictionary.set(element, dictionary.get(element)+1): dictionary.set(element,1))
-    return dictionary
-}
-
-console.log(countValue(ar2))
-// Задание №3
-
-class Student{
-    constructor(group){
-        this.group = group
+        return sum/count;
     }
 }
 
+function shellSort(students) {
+    const n = students.length;
+    let gap = Math.floor(n / 2);
 
-function getMaxGroup(array){
-    let groups = new Set();
-    let dictionary = new Map();
-
-    for (let element of array){
-        groups.add(+(element.group))
-        if (dictionary.get(element.group)!==undefined){
-            dictionary.set(element.group,dictionary.get(element.group)+1)
+    while (gap > 0) {
+        for (let i = gap; i < n; i++) {
+            const temp = students[i];
+            let j;
+            for (j = i; j >= gap && (students[j - gap].average()) > temp.average(); j -= gap) { 
+                students[j] = students[j - gap];
+            }
+            students[j] = temp;
         }
-        else{
-            dictionary.set(element.group,1)
-        }
+        gap = Math.floor(gap / 2);
     }
 
-    const maxGroup = Math.max(...groups)
-    return dictionary.get(String(maxGroup))
+    return students;
 }
 
-const ar = [new Student("6204"),new Student("6201"),new Student("9302"),new Student("7204"),new Student("1201"),new Student("9302")]
-console.log(getMaxGroup(ar))
+const ar = [new Student("Тест","Тестов",[{subject:"Информатика",mark:2}]),
+            new Student("Вася","Васильев",[{subject:"Математика",mark:5}]),
+            new Student("Иван","Иванов",[{subject:"Физика",mark:4}]),
+            new Student("Сидор","Сидоров",[{subject:"Русский",mark:3}])]
+
+console.log(shellSort(ar))
+
+
+  
